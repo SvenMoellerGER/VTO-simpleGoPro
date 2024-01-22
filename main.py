23236997925609569569ed1b7ca6d207face4fc1
Sven Möller <77204076+SvenMoellerGER@users.noreply.github.com>
2024-01-22 16:47:29 +0100

    Funktionserweiterungen und Verschönerungen
    Startnummer und Versuch können eingegeben werden, optische Verschönerungen

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
 import tkinter as tk
 from datetime import datetime
-from goprocam import GoProCamera, constants
+from goprocam import GoProCamera
 import tkinter.messagebox as messagebox
+
+
+class GoProController:
+    def __init__(self):
+        self.camera = GoProCamera.GoPro()
+
+    def date_and_time(self):
+        now = datetime.now()
+        return now.strftime("%Y%m%d-%H%M%S")
+
+    def start_recording(self):
+        dt = self.date_and_time()
+        try:
+            print(f"Aufnahme gestartet um {dt}")
+            self.camera.video_settings("R1080p", "120")
+            self.camera.shoot_video(5)  # Aufnahme gestarten, 5 Sekunden
+            print(f"Aufnahme beendet um {self.date_and_time()}")
+        except Exception as e:
+            print(f"Fehler beim Starten der Aufnahme. Fehler: {str(e)}")
+            messagebox.showerror("Aufnahme Fehler", f"Fehler beim Starten der Aufnahme. Fehler: {str(e)}")
+
+    def download_latest_video(self, stnr, versuch):
+        dt = self.date_and_time()
+        try:
+            self.camera.downloadLastMedia(custom_filename=f"{dt}-{stnr}-V{versuch}.MP4")
+            print(f"Neuestes Video erfolgreich heruntergeladen um {dt}")
+        except Exception as e:
+            print(f"Fehler beim Herunterladen des neuesten Videos. Fehler: {str(e)}")
+            messagebox.showerror("Download Fehler", f"Fehler beim Herunterladen des neuesten Videos. Fehler: {str(e)}")
 
 
 class GoProControllerGUI:
     def __init__(self, master):
         self.master = master
-        master.title("GoPro Controller")
+        master.title("Absprungskontrolle für Horizontalsprünge")
 
-        self.camera = GoProCamera.GoPro()
+        self.gopro_controller = GoProController()
 
-        # Load the image
-        image_path = "longjump.png"  # Replace with the actual image file path
+        # Bild laden
+        image_path = "resources/LogoVTO.png"
         original_image = tk.PhotoImage(file=image_path)
 
-        # Resize the image to have a maximum width of 400 pixels
-        max_width = 300
+        # Bild auf eine maximale Breite von 400 Pixel skalieren
+        max_width = 400
         image_width = original_image.width()
         if image_width > max_width:
             scale_factor = max_width / image_width
@@ -24,53 +53,55 @@ class GoProControllerGUI:
         else:
             self.logo_image = original_image
 
-        # Display the image using a Label with increased pady
         self.logo_label = tk.Label(master, image=self.logo_image)
-        self.logo_label.pack(pady=20)  # Increase space between image and buttons
+        self.logo_label.grid(row=0, column=0, columnspan=4, pady=20,
+                             sticky="nsew")
 
-        # Adjust the width and height of the buttons
-        button_width = 25
+        # Beschriftungen und Eingabefelder für "StNr" und "Versuch Nr." erstellen
+        label_font = ('Arial', 12, 'bold')  # Schriftgröße verdoppeln
+        entry_font = ('Arial', 10)  # Schriftgröße für Eingabefelder
+
+        self.versuch_label = tk.Label(master, text="Versuch Nr.:", font=label_font)
+        self.versuch_label.grid(row=1, column=0, padx=(10, 5), pady=10, sticky="e")
+        self.versuch_entry = tk.Entry(master, font=entry_font, width=10, justify="center", bd=2, relief="groove")
+        self.versuch_entry.grid(row=1, column=1, padx=(0, 10), pady=10, sticky="w")
+
+        self.stnr_label = tk.Label(master, text="StNr:", font=label_font)
+        self.stnr_label.grid(row=1, column=2, padx=(10, 5), pady=10, sticky="e")
+        self.stnr_entry = tk.Entry(master, font=entry_font, width=10, justify="center", bd=2, relief="groove")
+        self.stnr_entry.grid(row=1, column=3, padx=(0, 10), pady=10, sticky="w")
+
+        button_width = 20
         button_height = 2
 
-        self.record_button = tk.Button(master, text="Start Recording", command=self.start_recording, width=button_width,
-                                       height=button_height)
-        self.record_button.pack(pady=10)  # Add space between buttons
+        self.record_button = tk.Button(master, text="Aufnahme starten", font=entry_font,
+                                       command=self.gopro_controller.start_recording,
+                                       width=button_width, height=button_height, bg="#ffb5c5")
+        self.record_button.grid(row=2, column=0, padx=30, pady=15, columnspan=4, sticky="nsew")
 
-        self.download_button = tk.Button(master, text="Download Latest Video", command=self.download_latest_video,
-                                         width=button_width, height=button_height)
-        self.download_button.pack(pady=10)  # Add space between buttons
+        self.download_button = tk.Button(master, text="Neuestes Video herunterladen", font=entry_font,
+                                         command=lambda: self.gopro_controller.download_latest_video(
+                                             self.stnr_entry.get(), self.versuch_entry.get()),
+                                         width=button_width, height=button_height, bg="#ffff00")
+        self.download_button.grid(row=3, column=0, padx=30, pady=15, columnspan=4, sticky="nsew")
 
-        self.exit_button = tk.Button(master, text="Exit", command=self.exit_application, width=button_width,
-                                     height=button_height)
-        self.exit_button.pack(pady=10)  # Add space between buttons
+        self.exit_button = tk.Button(master, text="Beenden", font=entry_font, command=self.master.destroy,
+                                     width=button_width,
+                                     height=button_height, bg="#90ee90")
+        self.exit_button.grid(row=4, column=0, padx=30, pady=15, columnspan=4, sticky="nsew")
 
-        # Set the minimum size of the window
-        master.minsize(500, 300)
+        # Minimale Größe des Fensters setzen
+        master.minsize(500, 600)
 
-    def dateandtime(self):
-        now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        return dt_string
-
-    def start_recording(self):
-        dt = self.dateandtime()
-        print(f"Started recording at {dt}")
-        self.camera.shoot_video(5)  # Start recording, 5 secs
-
-    def download_latest_video(self):
-        dt = self.dateandtime()
-        try:
-            self.camera.downloadLastMedia()
-            print(f"Downloaded latest video successfully {dt}")
-        except Exception as e:
-            print(f"Failed to download latest video. Error: {str(e)}")
-            messagebox.showerror("Download Error", f"Failed to download latest video. Error: {str(e)}")
-
-    def exit_application(self):
-        self.master.destroy()
+        # Zeilen- und Spaltenverhältnisse für die Größenänderung konfigurieren
+        for i in range(5):  # Anzahl der Zeilen
+            master.grid_rowconfigure(i, weight=1)
+        for i in range(4):  # Anzahl der Spalten
+            master.grid_columnconfigure(i, weight=1)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.iconbitmap("resources/picto.ico")
     app = GoProControllerGUI(root)
     root.mainloop()
